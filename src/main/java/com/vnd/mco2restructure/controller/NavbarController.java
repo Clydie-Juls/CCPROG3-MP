68497d1d7fd856ad24b7a43086399741bd87efbc
UnityGrave <72306255+UnityGrave@users.noreply.github.com>
2023-07-31 17:51:35 +0800

    Add JavaDoc NavbarController

--- src/main/java/com/vnd/mco2restructure/controller/NavbarController.java
+++ src/main/java/com/vnd/mco2restructure/controller/NavbarController.java
@@ -3,26 +3,38 @@ package com.vnd.mco2restructure.controller;
 import com.vnd.mco2restructure.WindowManager;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * Controller class for the navigation bar.
+ */
 public class NavbarController {
 
     private WindowManager windowManager;
 
+    /**
+     * Method to navigate to the Vending Features View.
+     */
     @FXML
     private void gotoVndFeaturesView() {
         Platform.runLater(() -> windowManager.gotoVndFeaturesView());
     }
 
+    /**
+     * Method to navigate to the Maintenance Features View.
+     */
     @FXML
     private void gotoMntFeaturesView() {
         Platform.runLater(() -> windowManager.gotoCurrentMntLayout());
     }
 
+    /**
+     * Sets the WindowManager instance for this controller.
+     *
+     * @param windowManager The WindowManager instance to set.
+     */
     public void setWindowManager(WindowManager windowManager) {
         this.windowManager = windowManager;
     }
