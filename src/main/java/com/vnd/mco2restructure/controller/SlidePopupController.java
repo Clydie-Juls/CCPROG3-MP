012be558b5a1ed90b38f743d4d14ab52f67c1fae
UnityGrave <72306255+UnityGrave@users.noreply.github.com>
2023-07-31 17:54:51 +0800

    Add JavaDoc SlidePopupController

--- src/main/java/com/vnd/mco2restructure/controller/SlidePopupController.java
+++ src/main/java/com/vnd/mco2restructure/controller/SlidePopupController.java
@@ -4,11 +4,16 @@ import com.vnd.mco2restructure.component.SlidePopup;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
 
+/**
+ * Controller class for the SlidePopup component.
+ */
 public class SlidePopupController {
-
 
     @FXML public BorderPane layout;
 
+    /**
+     * Method to exit from the SlidePopup view.
+     */
     @FXML
     private void exit() {
         ((SlidePopup)layout).slideDownAnimation();
