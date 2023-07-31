3d18b001a2d0e770406a732e441e1c6f674d00e5
UnityGrave <72306255+UnityGrave@users.noreply.github.com>
2023-07-31 17:35:38 +0800

    Add JavaDoc ItemBuyController

--- src/main/java/com/vnd/mco2restructure/controller/ItemBuyController.java
+++ src/main/java/com/vnd/mco2restructure/controller/ItemBuyController.java
@@ -23,6 +23,9 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * Controller class for the item buy view.
+ */
 public class ItemBuyController {
     @FXML private Button buyItemButton;
     @FXML private SlidePopup slidePopup;
@@ -31,29 +34,38 @@ public class ItemBuyController {
     @FXML private Label itemName;
     private WindowManager windowManager;
     private ProgramData programData;
+
+    /**
+     * Handles the action when the "Exit" button is clicked.
+     * Goes back to the vending machine features view.
+     */
     @FXML
     private void exit() {
         windowManager.gotoVndFeaturesView();
     }
 
+    /**
+     * Updates the view to display the available items for customization and provides buttons to buy items.
+     *
+     * @param slotIndex The index of the slot in the vending machine.
+     */
     public void updateView(int slotIndex) {
         ArrayList<ToggleGroup> itemsChose = new ArrayList<>();
 
         infoLayout.getChildren().clear();
         Slot<? extends Item> slot = programData.getCurrentVendingMachine().getSlots()[slotIndex];
         itemName.setText(slot.getItem().getName());
-        if(programData.getCurrentVendingMachine() instanceof SpecialVendingMachine specialVendingMachine) {
-            if(slot.getItem() instanceof CustomizableItem customizableItem) {
+        if (programData.getCurrentVendingMachine() instanceof SpecialVendingMachine specialVendingMachine) {
+            if (slot.getItem() instanceof CustomizableItem customizableItem) {
                 itemImageView.setImage(new Image(
-                        Objects.requireNonNull(HelloApplication.class.
-                                getResourceAsStream(customizableItem.getImageFile()))));
+                        Objects.requireNonNull(HelloApplication.class.getResourceAsStream(customizableItem.getImageFile()))));
                 for (Map.Entry<String, NonCustomizableItem[]> entry : customizableItem.getItemsDerived().entrySet()) {
                     ItemChoices itemChoices = new ItemChoices();
                     ToggleGroup toggleGroup = new ToggleGroup();
                     itemChoices.getItemTypeLabel().setText(entry.getKey());
                     for (NonCustomizableItem nonCustomizableItem : entry.getValue()) {
-                        if(specialVendingMachine.getItemStorage().containsKey(nonCustomizableItem)) {
-                            if(specialVendingMachine.getItemStorage().get(nonCustomizableItem).size() > 0) {
+                        if (specialVendingMachine.getItemStorage().containsKey(nonCustomizableItem)) {
+                            if (specialVendingMachine.getItemStorage().get(nonCustomizableItem).size() > 0) {
                                 RadioButton itemChoice = new RadioButton(nonCustomizableItem.getName());
                                 itemChoice.setToggleGroup(toggleGroup);
                                 itemChoices.getItemChoicesLayout().getChildren().add(itemChoice);
@@ -70,12 +82,17 @@ public class ItemBuyController {
         }
     }
 
+    /**
+     * Buys the selected items and goes to the payment view.
+     *
+     * @param itemsChose The list of ToggleGroups for each item category.
+     * @param itemToBuy The customizable item to buy.
+     */
     public void buyItem(ArrayList<ToggleGroup> itemsChose, CustomizableItem itemToBuy) {
         NonCustomizableItem[] items = new NonCustomizableItem[itemsChose.size()];
         int i = 0;
         for (Map.Entry<String, NonCustomizableItem[]> entry : itemToBuy.getItemsDerived().entrySet()) {
-            items[i] = entry.getValue()[itemsChose.get(i).
-                    getToggles().indexOf(itemsChose.get(i).getSelectedToggle())];
+            items[i] = entry.getValue()[itemsChose.get(i).getToggles().indexOf(itemsChose.get(i).getSelectedToggle())];
             i++;
         }
         itemToBuy.setItemContents(items);
@@ -88,12 +105,21 @@ public class ItemBuyController {
         slidePopup.slideUpAnimation();
     }
 
+    /**
+     * Sets the WindowManager for this controller.
+     *
+     * @param windowManager The WindowManager instance.
+     */
     public void setWindowManager(WindowManager windowManager) {
         this.windowManager = windowManager;
     }
 
+    /**
+     * Sets the ProgramData for this controller.
+     *
+     * @param programData The ProgramData instance.
+     */
     public void setProgramData(ProgramData programData) {
         this.programData = programData;
     }
-
 }
