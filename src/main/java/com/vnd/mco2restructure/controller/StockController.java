9616ccdd68210f8656994c4e6f625b445fd1db17
UnityGrave <72306255+UnityGrave@users.noreply.github.com>
2023-07-31 17:56:02 +0800

    Add JavaDoc StockController

--- src/main/java/com/vnd/mco2restructure/controller/StockController.java
+++ src/main/java/com/vnd/mco2restructure/controller/StockController.java
@@ -4,24 +4,33 @@ import com.vnd.mco2restructure.WindowManager;
 import com.vnd.mco2restructure.menu.CustomizableItemEnum;
 import com.vnd.mco2restructure.menu.IndependentItemEnum;
 import com.vnd.mco2restructure.model.StockData;
+import com.vnd.mco2restructure.component.NumberField;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 
+/**
+ * Controller class for handling stocks.
+ */
 public class StockController {
 
-    private StockData stockData;
     private WindowManager windowManager;
+    private StockData stockData;
     @FXML private FlowPane menuLayout;
 
+    /**
+     * Sets the view for the StockController based on the vending machine type.
+     *
+     * @param isSpecialVendingMachine True if the vending machine is special, false otherwise.
+     */
     public void setView(boolean isSpecialVendingMachine) {
         menuLayout.getChildren().clear();
-        if(isSpecialVendingMachine) {
+        if (isSpecialVendingMachine) {
             for (CustomizableItemEnum item : CustomizableItemEnum.values()) {
-                //TODO: create fxml structure for this
-                Label itemName = new Label(item.toString().toLowerCase().replace('_',' '));
+                // TODO: create fxml structure for this
+                Label itemName = new Label(item.toString().toLowerCase().replace('_', ' '));
                 VBox vBox = new VBox(itemName);
                 vBox.setStyle("-fx-background-color: #aaaaaa;");
 
@@ -36,8 +45,8 @@ public class StockController {
         }
 
         for (IndependentItemEnum item : IndependentItemEnum.values()) {
-            //TODO: create fxml structure for this
-            Label itemName = new Label(item.toString().toLowerCase().replace('_',' '));
+            // TODO: create fxml structure for this
+            Label itemName = new Label(item.toString().toLowerCase().replace('_', ' '));
             VBox vBox = new VBox(itemName);
             vBox.setStyle("-fx-background-color: #aaaaaa;");
             menuLayout.getChildren().add(vBox);
@@ -51,19 +60,37 @@ public class StockController {
         }
     }
 
+    /**
+     * Sets the slot ID for the stock.
+     *
+     * @param slotId The slot ID to set.
+     */
     public void setSlotId(int slotId) {
         stockData.setSlotId(slotId);
     }
 
+    /**
+     * Method to exit from the StockController view.
+     */
     @FXML
     private void exit() {
         windowManager.gotoStockManagerView(false);
     }
 
+    /**
+     * Sets the WindowManager instance for this controller.
+     *
+     * @param windowManager The WindowManager instance to set.
+     */
     public void setWindowManager(WindowManager windowManager) {
         this.windowManager = windowManager;
     }
 
+    /**
+     * Sets the stock data for the StockController.
+     *
+     * @param stockData The stock data to set.
+     */
     public void setStockData(StockData stockData) {
         this.stockData = stockData;
     }
