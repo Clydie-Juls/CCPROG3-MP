632bb623073d683c23cfb4918adadd496d6ce56e
UnityGrave <72306255+UnityGrave@users.noreply.github.com>
2023-08-01 20:13:45 +0800

    Add JavaDoc CustomizableItem

--- src/main/java/com/vnd/mco2restructure/model/items/CustomizableItem.java
+++ src/main/java/com/vnd/mco2restructure/model/items/CustomizableItem.java
@@ -1,37 +1,66 @@
 package com.vnd.mco2restructure.model.items;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Arrays;
 
-public class CustomizableItem extends Item{
+/**
+ * The CustomizableItem class represents a customizable item in the vending machine.
+ * It extends the Item class and implements the Cloneable interface.
+ */
+public class CustomizableItem extends Item {
 
     private HashMap<String, NonCustomizableItem[]> itemsDerived;
     private NonCustomizableItem[] itemContents;
+
     /**
-     * Constructs an Item object with the specified name, calories, and price.
+     * Constructs a CustomizableItem object with the specified name, itemsDerived, id, and image file.
      *
-     * @param name     The name of the item.
+     * @param name         The name of the customizable item.
+     * @param itemsDerived The mapping of item types to their corresponding non-customizable items.
+     * @param id           The unique identifier of the customizable item.
+     * @param imageFile    The file path of the image for the customizable item.
      */
-    public CustomizableItem(String name,  HashMap<String, NonCustomizableItem[]> itemsDerived, int id, String imageFile) {
+    public CustomizableItem(String name, HashMap<String, NonCustomizableItem[]> itemsDerived, int id, String imageFile) {
         super(name, 0, id, 0, imageFile);
         this.itemsDerived = itemsDerived;
     }
 
+    /**
+     * Gets the mapping of item types to their corresponding non-customizable items.
+     *
+     * @return The mapping of item types to their corresponding non-customizable items.
+     */
     public HashMap<String, NonCustomizableItem[]> getItemsDerived() {
         return itemsDerived;
     }
 
+    /**
+     * Gets the array of non-customizable items contained in the customizable item.
+     *
+     * @return The array of non-customizable items.
+     */
     public NonCustomizableItem[] getItemContents() {
         return itemContents;
     }
 
+    /**
+     * Sets the array of non-customizable items contained in the customizable item.
+     * It also updates the total price and calories of the customizable item based on the item contents.
+     *
+     * @param itemContents The array of non-customizable items to be set for the customizable item.
+     */
     public void setItemContents(NonCustomizableItem[] itemContents) {
         this.itemContents = itemContents;
         setPrice(Arrays.stream(itemContents).mapToInt(Item::getPrice).sum());
         setCalories(Arrays.stream(itemContents).mapToInt(Item::getCalories).sum());
     }
 
+    /**
+     * Gets the total price of the customizable item based on the item contents.
+     * The price is calculated as the sum of the prices of all non-customizable items.
+     *
+     * @return The total price of the customizable item.
+     */
     @Override
     public int getPrice() {
         price = 0;
@@ -41,6 +70,12 @@ public class CustomizableItem extends Item{
         return price;
     }
 
+    /**
+     * Creates a clone of the customizable item.
+     * It also copies the mutable state to prevent changes to the original item.
+     *
+     * @return The cloned CustomizableItem object.
+     */
     @Override
     public CustomizableItem clone() {
         // TODO: copy mutable state here, so the clone can't change the internals of the original
