e6c290fa955b079a3091fe74c41c64a11961bdc8
UnityGrave <72306255+UnityGrave@users.noreply.github.com>
2023-08-01 20:21:04 +0800

    Add JavaDoc StorageSlot

--- src/main/java/com/vnd/mco2restructure/model/slots/StorageSlot.java
+++ src/main/java/com/vnd/mco2restructure/model/slots/StorageSlot.java
@@ -1,15 +1,17 @@
 package com.vnd.mco2restructure.model.slots;
 
-
 import com.vnd.mco2restructure.model.items.NonCustomizableItem;
 
 import java.util.ArrayDeque;
 
-public class StorageSlot extends Slot<NonCustomizableItem>{
-
+/**
+ * The StorageSlot class represents a slot in the vending machine's storage that holds non-customizable items.
+ */
+public class StorageSlot extends Slot<NonCustomizableItem> {
     private final ArrayDeque<NonCustomizableItem> itemStack;
+
     /**
-     * Constructs a Slot object with the specified capacity.
+     * Constructs a StorageSlot object with the specified capacity.
      *
      * @param capacity The maximum capacity of the slot for holding items.
      */
@@ -18,24 +20,38 @@ public class StorageSlot extends Slot<NonCustomizableItem>{
         itemStack = new ArrayDeque<>();
     }
 
+    /**
+     * Puts an item into the storage slot.
+     * If the item is the same as the one already in the slot, it is stacked on top.
+     *
+     * @param item The non-customizable item to be put into the slot.
+     */
     @Override
     public void putItem(NonCustomizableItem item) {
-        if(itemStack.isEmpty()) {
-            itemStack.add(item);
-            super.item = item;
-        } else if(item.equals(itemStack.peek())) {
+        if (itemStack.isEmpty() || item.equals(itemStack.peek())) {
             itemStack.add(item);
             super.item = item;
         }
     }
 
+    /**
+     * Removes and returns the top item from the storage slot.
+     * If there's only one item left, it will also clear the slot.
+     *
+     * @return The top non-customizable item in the slot.
+     */
     public NonCustomizableItem giveItem() {
-        if(itemStack.size() == 1) {
+        if (itemStack.size() == 1) {
             item = null;
         }
         return itemStack.pollLast();
     }
 
+    /**
+     * Retrieves the number of items stacked in the storage slot.
+     *
+     * @return The count of items stacked in the slot.
+     */
     public int getItemStackCount() {
         return itemStack.size();
     }
